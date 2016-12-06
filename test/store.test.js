a40035286fcb062889f166b2ca4b7e2882dbd07a
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2016-12-06 23:50:22 +0000

    make it better

---
+++ test/store.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import expect from 'expect';
+import configureMockStore from 'redux-mock-store';
+import createSagaMiddleware from 'redux-saga';
+import { searchMediaAction } from '../src/actions/mediaActions';
+
+const sagaMiddleware = createSagaMiddleware();
+const mockStore = configureMockStore([sagaMiddleware]);
+
+describe('Test store', () => {
+  it('should return dispatch action to the api', () => {
+    const store = mockStore({});
+    const expectedValue = {
+      type: 'SEARCH_MEDIA_REQUEST',
+      payload: 'rain'
+    };
+    store.dispatch(searchMediaAction('rain'));
+    expect(store.getActions()).toEqual([expectedValue]);
+  });
+});
